9f0e15b9d3e574942abe093ffa8d455ec833f741
sijabulile-ncube <s.mabhena@gmail.com>
2026-07-13 21:19:56 +0200

    added comments on file: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// grabbing the root div from index.html and mounting the app into it
 createRoot(document.getElementById('root')).render(
+  // StrictMode helps catch potential bugs during dev, doesn't affect production
   <StrictMode>
     <App />
   </StrictMode>,
